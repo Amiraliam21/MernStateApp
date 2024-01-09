b3ae4b91b429b93dd36b95ee186f9b549d8861d2
Amiraliam21 <amirali.mohammadi0067@gmail.com>
2024-01-09 18:25:35 +0100

    test Api ROUTE

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express()
 const port = 4000
+
+import mongoose from 'mongoose'
+import router from './routes/user.route.js';
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to MongoDB!")
+    }).catch((err) =>{
+        console.log(err);
+    });
+    
+    app.use('/api/user', router);
+
+router.get('/test',(req,res)=>{
+    res.send('Hello world')
+})
+
 
 
 
